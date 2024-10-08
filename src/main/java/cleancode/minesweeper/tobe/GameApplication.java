d3fc3ee6d729f38394ba3cfc55cd55d4c443347f
sskkilm <sskkilm@naver.com>
2024-10-08 20:22:34 +0900

    refactor: Dependency Inversion Principle

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -2,13 +2,19 @@ package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.gamelevel.Middle;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
 
     public static void main(String[] args) {
         GameLevel gameLevel = new Middle();
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
 
-        MineSweeper mineSweeper = new MineSweeper(gameLevel);
+        MineSweeper mineSweeper = new MineSweeper(gameLevel, inputHandler, outputHandler);
         mineSweeper.initialize();
         mineSweeper.run();
     }
